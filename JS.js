b3e1dd5388b90aa9bf6c4a68e4181bb70f31166a
Elizaveta Gaidarova <gaidarova.liza@gmail.com>
2023-03-25 12:01:35 +0300

    commit

--- JS.js
+++ JS.js
@@ -17,3 +17,23 @@ function isPalindrome(x) {
 	x = x.toLowerCase()
 	return x === x.split``.reverse().join``// is it palindrom
 }
+
+const humanYearsCatYearsDogYears = humanYears => [
+	humanYears,
+	(humanYears - 1 ? 16 : 11) + 4 * humanYears,
+	(humanYears - 1 ? 14 : 10) + 5 * humanYears,
+];
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
+
+const summation = n => n * (n + 1) / 2;
+
+switchItUp = n => ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][n]
+
+String.prototype.toAlternatingCase = function () {
+	return this.split("").map(a => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join('')
+}
+
+
+const hotpo = (n, acc = 0) =>
+	n <= 1 ? acc : hotpo(n % 2 === 0 ? n / 2 : 3 * n + 1, acc + 1)
