184020ec4eb4e8a6eff2b8118a1d2259f7014e75
Elizaveta Gaidarova <gaidarova.liza@gmail.com>
2023-03-25 11:38:28 +0300

    commit

--- JS.js
+++ JS.js
@@ -5,3 +5,15 @@ function joinStrings(string1, string2) {
 const grow = x => x.reduce((a, b) => a * b); //Beginner - Reduce but Grow.js
 
 return s.replace(/!/g, ''); //Remove exclamation marks.js 
+
+const min = (list) => Math.min(...list); //Find Maximum and Minimum Values of a List
+const max = (list) => Math.max(...list);
+
+function shortcut(string) {
+	return string.replace(/[aeiou]/g, '')
+}
+
+function isPalindrome(x) {
+	x = x.toLowerCase()
+	return x === x.split``.reverse().join``// is it palindrom
+}
